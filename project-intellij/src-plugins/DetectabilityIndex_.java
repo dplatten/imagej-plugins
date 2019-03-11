979dacf6bcd59de7a4643abe4f763341fbdcfdb8
David Platten <david.platten@ngh.nhs.uk>
2019-03-11 19:03:19 +0000

    Updated

--- project-intellij/src-plugins/DetectabilityIndex_.java
+++ project-intellij/src-plugins/DetectabilityIndex_.java
@@ -11,24 +11,30 @@ import ij.process.ImageProcessor;
 public class DetectabilityIndex_ implements PlugInFilter {
     private ImagePlus imp;
 
-	public int setup(String arg, ImagePlus imp) {
-    	this.imp = imp;
+    public int setup(String arg, ImagePlus imp) {
+        this.imp = imp;
 
-    	if(this.imp == null) {
-			IJ.error("Please open an image");
-			return(0);
-		}
+        if(this.imp == null) {
+            IJ.error("Please open an image");
+            return(0);
+        }
 
-    	return DOES_ALL;
+        return DOES_ALL;
     }
 
     public void run(ImageProcessor ip) {
-		// First run the TTF analysis
-		TaskTransferFunction_ ttf = new TaskTransferFunction_();
-		ttf.setup("defaults", imp);
-		ttf.run(ip);
+        // Detectability index needs
+        //  Task Transfer Function (TTF)
+        //  Noise Power Spectrum (NPS)
+        //  Eye model (E)
+        //  Task function (TF)
 
-		TaskTransferFunction_.TTF_result ttf_results = ttf.getTTFResult();
-		TaskTransferFunction_.CNR_result cnr_results = ttf.getCNRResult();
-	}
+        // First run the TTF analysis
+        TaskTransferFunction_ ttf = new TaskTransferFunction_();
+        ttf.setup("defaults", imp);
+        ttf.run(ip);
+
+        TaskTransferFunction_.TTF_result ttf_results = ttf.getTTFResult();
+        TaskTransferFunction_.CNR_result cnr_results = ttf.getCNRResult();
+    }
 }
